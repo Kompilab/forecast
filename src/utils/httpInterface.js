17876c024e0307af7c767ff2f07743a64bfcac5a
ceemion <fiyinsimeon@gmail.com>
2019-05-03 19:25:10 +0100

    Remove proxy, manually configure environment urls

--- src/utils/httpInterface.js
+++ src/utils/httpInterface.js
@@ -1,6 +1,12 @@
 import Cookie from 'universal-cookie';
-
 const cookie = new Cookie();
+
+const envUrls = {
+  development: 'http://localhost:5001',
+  production: 'https://my-forecast-api.herokuapp.com'
+}
+
+const BASE_URL = envUrls[process.env.NODE_ENV]
 
 let headers = new Headers();
 headers.append("Content-Type", "application/json; charset=utf-8");
@@ -8,16 +14,16 @@ headers.append("Authorization", cookie.get('_fo_'));
 
 const httpInterface = {
   getData(url) {
-    return fetch(url, {
+    return fetch(`${BASE_URL}${url}`, {
       headers: headers
     })
   },
   postData(url, method, payload={}) {
-    return fetch(url, {
+    return fetch(`${BASE_URL}${url}`, {
       method: method,
-      mode: "same-origin",
+      mode: "cors",// no-cors, cors, *same-origin
       cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: "same-origin", // include, same-origin, *omit
+      credentials: "include", // include, same-origin, *omit
       headers: headers,
       body: JSON.stringify(payload)
     })
