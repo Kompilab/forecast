86fabaa6e63cac74dd1f1ae59bb318c8df7c8cee
ceemion <fiyinsimeon@gmail.com>
2019-05-29 09:57:34 +0100

    WIP temporarily move bank statement upload form to toggle view

--- src/pages/Dashboard/Views/BankStatements/BankStatements.jsx
+++ src/pages/Dashboard/Views/BankStatements/BankStatements.jsx
@@ -220,65 +220,6 @@ class BankStatements extends Component {
 
     return (
       <div>
-        <section className="upload mb-5 container-fluid">
-          <div className="row">
-            <div className="col-12 summary-card p-3">
-              <h5>Upload bank statement</h5>
-
-              {
-                formErrors ? (
-                  <div>{FormattersHelpers.formatErrors(formErrors)}</div>
-                ): null
-              }
-
-              <form className="upload-form">
-                <div className="form-row">
-                  <div className="col-sm-4">
-                    <select className="custom-select mb-2" id="bankSelect" name="selectedBank" onChange={this.handleChange} required>
-                      <option value="">Choose your bank</option>
-                      {
-                        supportedBanks && supportedBanks.map((bank, index) => {
-                          return <option value={index} key={index}>{bank.name}</option>
-                        })
-                      }
-                    </select>
-                    { selectedBank && <small id="bankHelp" className="form-text text muted mb-2">Allowed file formats for this bank: {allowedFormats}</small> }
-                  </div>
-                  <div className="col-sm-4">
-                    <div className="input-group mb-2">
-                      <div className="custom-file">
-                        <input name="uploadedFile" type="file" accept=".pdf,.xls,.xlsx" className="custom-file-input" id="chooseStatementFile" onChange={this.handleOnFileChange} aria-describedby="inputGroupFileAddonBankStatement" />
-                        <label className="custom-file-label" htmlFor="chooseStatementFile">
-                          { (uploadedFile === undefined || uploadedFile === null) || (uploadedFile && !uploadedFile.name) ? 'Choose file' : (uploadedFile && uploadedFile.name) }</label>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-sm-4">
-                    <div className="form-group mb-2">
-                      <input id="filePassword" type="password" name="filePassword" className="form-control" onChange={this.handleChange} placeholder="File password (optional)" />
-                    </div>
-                  </div>
-                </div>
-
-                <div className="actions my-3 text-center">
-                  <button onClick={this._handleFileUpload} type="submit" className="btn btn-primary btn-fo-primary" disabled={this.disableUpload()}>
-                    {
-                      uploading ? (
-                        <div>
-                          <span className="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
-                          Uploading...
-                        </div>
-                      ) : (
-                        <div>Upload</div>
-                      )
-                    }
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </section>
-
         <section className="all-statements fo-section mb-3">
           <div className="header mb-3">
             <div className="">
@@ -310,6 +251,69 @@ class BankStatements extends Component {
               </button>
             </div>
           </div>
+
+          {
+            formOpen ? (
+              <section className="upload mb-5 container-fluid">
+                <div className="row">
+                  <div className="col-12 summary-card p-3">
+                    <h5>Upload bank statement</h5>
+
+                    {
+                      formErrors ? (
+                        <div>{FormattersHelpers.formatErrors(formErrors)}</div>
+                      ): null
+                    }
+
+                    <form className="upload-form">
+                      <div className="form-row">
+                        <div className="col-sm-4">
+                          <select className="custom-select mb-2" id="bankSelect" name="selectedBank" onChange={this.handleChange} required>
+                            <option value="">Choose your bank</option>
+                            {
+                              supportedBanks && supportedBanks.map((bank, index) => {
+                                return <option value={index} key={index}>{bank.name}</option>
+                              })
+                            }
+                          </select>
+                          { selectedBank && <small id="bankHelp" className="form-text text muted mb-2">Allowed file formats for this bank: {allowedFormats}</small> }
+                        </div>
+                        <div className="col-sm-4">
+                          <div className="input-group mb-2">
+                            <div className="custom-file">
+                              <input name="uploadedFile" type="file" accept=".pdf,.xls,.xlsx" className="custom-file-input" id="chooseStatementFile" onChange={this.handleOnFileChange} aria-describedby="inputGroupFileAddonBankStatement" />
+                              <label className="custom-file-label" htmlFor="chooseStatementFile">
+                                { (uploadedFile === undefined || uploadedFile === null) || (uploadedFile && !uploadedFile.name) ? 'Choose file' : (uploadedFile && uploadedFile.name) }</label>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="col-sm-4">
+                          <div className="form-group mb-2">
+                            <input id="filePassword" type="password" name="filePassword" className="form-control" onChange={this.handleChange} placeholder="File password (optional)" />
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="actions my-3 text-center">
+                        <button onClick={this._handleFileUpload} type="submit" className="btn btn-primary btn-fo-primary" disabled={this.disableUpload()}>
+                          {
+                            uploading ? (
+                              <div>
+                                <span className="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
+                                Uploading...
+                              </div>
+                            ) : (
+                              <div>Upload</div>
+                            )
+                          }
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </section>
+            ) : null
+          }
 
           {
             errors ? (
